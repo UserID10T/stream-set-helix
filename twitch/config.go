b7b8a4c356a0754c33e763fc276d8aa59e7ced42
UserID10T <userid10tgaming@gmail.com>
2026-07-22 17:31:55 -0500

    Replace twitch package with coherent Helix models and config

--- twitch/config.go
+++ twitch/config.go
@@ -22,10 +22,64 @@ var (
 	UserChannel Channel
 )
 
-type (
-	DBGame struct {
-		TwitchName       string   `json:"twitchName"`
-		FilePath         string   `json:"filePath"`
-		AlternativeNames []string `json:"alternativeName"`
-	}
-)
+type DBGame struct {
+	TwitchName       string   `json:"twitchName"`
+	FilePath         string   `json:"filePath"`
+	AlternativeNames []string `json:"alternativeName"`
+}
+
+type UserResponse struct {
+	Data []User `json:"data"`
+}
+
+type User struct {
+	ID              string `json:"id"`
+	Login           string `json:"login"`
+	DisplayName     string `json:"display_name"`
+	Type            string `json:"type"`
+	BroadcasterType string `json:"broadcaster_type"`
+	Description     string `json:"description"`
+	ProfileImageURL string `json:"profile_image_url"`
+	OfflineImageURL string `json:"offline_image_url"`
+	ViewCount       int64  `json:"view_count"`
+	Email           string `json:"email,omitempty"`
+	CreatedAt       string `json:"created_at"`
+}
+
+type ChannelResponse struct {
+	Data []Channel `json:"data"`
+}
+
+type Channel struct {
+	BroadcasterID    string `json:"broadcaster_id"`
+	BroadcasterLogin string `json:"broadcaster_login"`
+	BroadcasterName  string `json:"broadcaster_name"`
+	BroadcasterLanguage string `json:"broadcaster_language"`
+	GameID           string `json:"game_id"`
+	GameName         string `json:"game_name"`
+	Title            string `json:"title"`
+	Delay            int    `json:"delay"`
+}
+
+type SearchCategoriesResponse struct {
+	Data []SearchCategory `json:"data"`
+}
+
+type SearchCategory struct {
+	ID        string `json:"id"`
+	Name      string `json:"name"`
+	BoxArtURL string `json:"box_art_url"`
+}
+
+type TopGamesResponse struct {
+	Data []TopGame `json:"data"`
+	Pagination struct {
+		Cursor string `json:"cursor"`
+	} `json:"pagination"`
+}
+
+type TopGame struct {
+	ID        string `json:"id"`
+	Name      string `json:"name"`
+	BoxArtURL string `json:"box_art_url"`
+}

c7d415e5ca864cd09a3f1090405410dfa10bcf12
UserID10T <userid10tgaming@gmail.com>
2026-07-22 16:56:47 -0500

    Migrate Twitch config to Helix API

--- twitch/config.go
+++ twitch/config.go
@@ -1,17 +1,18 @@
 package twitch
 
 const (
-	ClientID        string = "1jcuu1fyzg8nabsmoplijb826zoyte0"
-	RedirectURI     string = "http://localhost:8000/twitch/token/"
+	ClientID    string = "1jcuu1fyzg8nabsmoplijb826zoyte0"
+	RedirectURI string = "http://localhost:8000/twitch/token/"
+	// Use the implicit flow to keep the existing local redirect-based login flow working.
 	RequestTokenURL string = "" +
 		"https://id.twitch.tv/oauth2/authorize" +
 		"?response_type=token" +
 		"&client_id=" + ClientID +
 		"&redirect_uri=" + RedirectURI +
 		"&force_verify=true" +
-		"&scope=channel_editor+channel_read"
+		"&scope=channel:manage:broadcast+channel:read:redemptions"
 
-	TwitchAPIURL string = "https://api.twitch.tv/kraken"
+	TwitchAPIURL string = "https://api.twitch.tv/helix"
 )
 
 var (
